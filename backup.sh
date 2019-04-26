024a4b5a444ae0f78d3a8e32165de7e1f491622e
Adam Christie <fractos@gmail.com>
2019-04-26 12:46:27 +0100

    some improvements to ensure exit code

--- backup.sh
+++ backup.sh
@@ -31,20 +31,21 @@ docker run --rm ${ENGINE_FAMILY}:${ENGINE_VERSION} $BACKUP_COMMAND > $OUTPUT_FIL
 if [ ! -e $OUTPUT_FILE ];
 then
   announce "$DB_NAME backup: Output file is missing!"
-  exit
+  exit 1
 fi
 
 if [ -s $OUTPUT_FILE ];
 then
   announce "$DB_NAME backup: Output file was zero length!"
-  exit
+  exit 1
 fi
 
 echo "GZipping output..."
 gzip $OUTPUT_FILE
 echo "Transfer output to S3"
 aws s3 cp $OUTPUT_FILE_GZ $S3_PREFIX$FORMATTED_FILENAME.gz
-announce "$DB_NAME backup: Written to $S3_PREFIX$FORMATTED_FILENAME.gz"
 echo "Removing temporary file"
 rm -f $OUTPUT_FILE_GZ
 echo "Done"
+announce "$DB_NAME backup: Written to $S3_PREFIX$FORMATTED_FILENAME.gz"
+exit 0
